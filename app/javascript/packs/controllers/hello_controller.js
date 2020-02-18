eb03371a685e7fa42ecb9d2b70db3b881a30a381
Nkunzi Innocent <innocentnkunzi@yahoo.com>
2020-02-18 13:10:50 +0200

    how to use js methods

--- app/javascript/packs/controllers/hello_controller.js
+++ app/javascript/packs/controllers/hello_controller.js
@@ -4,4 +4,9 @@ export default class extends Controller{
   log(){
     console.log(this.targets.find("name").value)
   }
+
+  paste(event){
+    event.preventDefault()
+    console.log("paste not allowed")
+  }
 }
